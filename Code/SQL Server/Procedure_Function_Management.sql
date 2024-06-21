dcda51c132282b3c16f22511f9fb75df3a738469
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-06-21 23:56:19 +0200

    Add procedure to handle Oracle procedure and show details

--- Code/SQL Server/Procedure_Function_Management.sql
+++ Code/SQL Server/Procedure_Function_Management.sql
@@ -5,7 +5,7 @@ SELECT * FROM Transakcje_paliwowe;
 
 SELECT * FROM Transakcje_spozywcze;
 
-SELECT * 
+SELECT *
 FROM ZaopatrzenieOracle.."ADMINISTRATORORACLE"."PALIWA";
 go
 
@@ -41,12 +41,70 @@ END;
 EXEC show_transaction_analize_products_daily @product_id = 3;
 
 -- show history of specific fuel price
+CREATE OR ALTER VIEW view_price_history
+AS
+	SELECT ID_TYPU, DATA_CENY, CENA FROM OPENQUERY(ZaopatrzenieOracle,'SELECT * FROM historia_cen_paliw');
+
+
+CREATE OR ALTER PROCEDURE show_history_of_fuel_price(
+@fuel_id INT = 1)
+AS
+BEGIN
+	SELECT DATA_CENY, CENA FROM view_price_history
+	WHERE ID_TYPU = @fuel_id
+	ORDER BY DATA_CENY DESC;
+END;
+
+EXEC dbo.show_history_of_fuel_price @fuel_id = 1;
 
 -- check orders of products/fuel
+CREATE OR ALTER PROCEDURE show_orders_of_fuel(
+@fuel_id INT = 1)
+AS
+BEGIN
+	SELECT * 
+	FROM OPENQUERY(ZaopatrzenieOracle,'SELECT * FROM show_order_fuel_history')
+	WHERE ID_TYPU = @fuel_id;
+END;
+
+EXEC show_orders_of_fuel @fuel_id = 2;
 
 -- check number of customers
+CREATE OR ALTER PROCEDURE show_number_of_new_customer
+AS
+BEGIN
+	SELECT YEAR(DATA_ZALOZENIA_KONTA) AS 'Year', MONTH(DATA_ZALOZENIA_KONTA) AS 'Month',
+	COUNT(ID_KLIENTA) AS 'Number Of Customers'
+	FROM ZaopatrzenieOracle.."ADMINISTRATORORACLE"."KLIENCI"
+	GROUP BY YEAR(DATA_ZALOZENIA_KONTA), MONTH(DATA_ZALOZENIA_KONTA)
+	ORDER BY YEAR(DATA_ZALOZENIA_KONTA) DESC, MONTH(DATA_ZALOZENIA_KONTA) ASC;
+END;
+
+EXEC dbo.show_number_of_new_customer;
+
 
 -- update price of fuel
+CREATE OR ALTER PROCEDURE update_price_of_fuel(
+@fuel_name VARCHAR(MAX),
+@new_price NUMERIC(3,2)
+)
+AS
+BEGIN
+	DECLARE @sql NVARCHAR(MAX);
+	SET @sql = N'
+BEGIN 
+    update_petrol_price(''' + @fuel_name + ''', ' + CAST(@new_price AS NVARCHAR) + '); 
+END;';
+	EXECUTE(@sql) AT [ZaopatrzenieOracle]
+END;
+
+EXEC update_price_of_fuel @fuel_name = 'benzyna 95', @new_price = 3.22;
+
 
 -- update points petrol
-
+CREATE OR ALTER PROCEDURE update_points_of_fuel(
+@fuel_id INT = 1)
+AS
+BEGIN
+	
+END;

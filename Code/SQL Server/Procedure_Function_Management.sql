4c69efcdb2022cdb998f178a2bfb2d2ad8f813c1
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-06-21 22:40:02 +0200

    Add procedure to show products sells

--- Code/SQL Server/Procedure_Function_Management.sql
+++ Code/SQL Server/Procedure_Function_Management.sql
@@ -11,18 +11,34 @@ go
 
 -- show number and sum price of transactions daily (fuel)
 CREATE OR ALTER PROCEDURE show_transaction_analize_daily_fuel
+(@fuel_id INT = 1)
 AS
 BEGIN
 
-	SELECT COUNT(ID_transakcji) AS NumberOfTransaction, ID_paliwa, data_transakcji
+	SELECT data_transakcji, COUNT(ID_transakcji) AS NumberOfTransaction,
+	SUM(kwota_transakcji) AS SumOfTransaction, SUM(ilosc_paliwa) SumOfFuel
 	FROM Transakcje_paliwowe
-	GROUP BY data_transakcji, ID_paliwa;
+	WHERE ID_paliwa = @fuel_id
+	GROUP BY data_transakcji
+	ORDER BY data_transakcji DESC;
 
 END;
 
-EXEC show_transaction_analize_daily_fuel;
+EXEC show_transaction_analize_daily_fuel @fuel_id = 1;
 
 -- show number and sum price of transactions daily (products)
+CREATE OR ALTER PROCEDURE show_transaction_analize_products_daily
+(@product_id INT = 1)
+AS
+BEGIN
+	SELECT data_transakcji, COUNT(ID_transakcji) AS NumberOfTransaction, COUNT(ilosc) AS NumberOfSoldProducts, SUM(kwota_transakcji) AS SumOfTransaction
+	FROM Transakcje_spozywcze
+	WHERE ID_produktu = @product_id
+	GROUP BY data_transakcji
+	ORDER BY data_transakcji DESC;
+END;
+
+EXEC show_transaction_analize_products_daily @product_id = 3;
 
 -- show history of specific fuel price
 

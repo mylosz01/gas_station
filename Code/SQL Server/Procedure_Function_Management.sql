9f502c85c5d3be3fcfce520dd8e5700fda45ef26
Miłosz Wyrębkiewicz <240840@edu.p.lodz.pl>
2024-06-22 12:45:14 +0200

    Add procedures to show statistic about employee salary

--- Code/SQL Server/Procedure_Function_Management.sql
+++ Code/SQL Server/Procedure_Function_Management.sql
@@ -127,3 +127,54 @@ BEGIN
 	GROUP BY NAZWA_PROGRAMU, DATA_TRANSAKCJI
 	ORDER BY DATA_TRANSAKCJI DESC, NAZWA_PROGRAMU ASC;
 END;
+
+EXEC show_statistic_car_wash;
+
+-- show employee work hours by month
+CREATE OR ALTER PROCEDURE show_employee_hours
+(@emp_id INT = 1)
+AS
+BEGIN
+	
+	SELECT Prac.imie, Prac.Nazwisko, 
+	SUM(DATEDIFF(HOUR,data_rozpoczecia_zmiany,data_zakonczenia_zmiany)) AS TotalHourWork,
+	SUM(DATEDIFF(HOUR,data_rozpoczecia_zmiany,data_zakonczenia_zmiany)) * Prac.[pensja za godzinê] AS EstimatedSalary,
+	YEAR(data_rozpoczecia_zmiany) AS 'YEAR', MONTH(data_rozpoczecia_zmiany) AS 'Month'
+	FROM Harmonogram AS Harm
+	JOIN Pracownicy...[Pracownicy$] AS Prac
+	ON Prac.Id_pracownika = Harm.ID_pracownika
+	WHERE Harm.ID_pracownika = @emp_id
+	GROUP BY Prac.imie, Prac.Nazwisko, Prac.ID_pracownika,Prac.[pensja za godzinê],
+	YEAR(data_rozpoczecia_zmiany), MONTH(data_rozpoczecia_zmiany);
+
+END;
+
+EXEC show_employee_hours @emp_id = 1;
+
+-- show employee total salary cost
+CREATE OR ALTER PROCEDURE show_employee_works_all
+AS
+BEGIN
+	
+	SELECT temp.Year_, temp.Month_, SUM(temp.EstimatedSalary) AS TotalPaidPerMonth
+	FROM (
+		SELECT YEAR(data_rozpoczecia_zmiany) AS Year_, MONTH(data_rozpoczecia_zmiany) AS Month_,
+		SUM(DATEDIFF(HOUR,data_rozpoczecia_zmiany,data_zakonczenia_zmiany)) * Prac.[pensja za godzinê] AS EstimatedSalary
+		FROM Harmonogram AS Harm
+		JOIN Pracownicy...[Pracownicy$] AS Prac
+		ON Prac.Id_pracownika = Harm.ID_pracownika
+		GROUP BY Prac.[pensja za godzinê],
+		YEAR(data_rozpoczecia_zmiany), MONTH(data_rozpoczecia_zmiany)) AS temp
+		GROUP BY temp.Year_, temp.Month_;
+
+END;
+
+EXEC show_employee_works_all;
+
+SELECT * FROM Pracownicy...[Pracownicy$];
+
+USE StacjaZarzad;
+USE StacjaPaliw;
+GO
+
+EXEC [dbo].[add_shift_employee] @id_emp = 1, @shift_date = '2024-07-01 15:00:00';
